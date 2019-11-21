fa94cb71685b97ecaa638621e87bc9c58d4b8d38
Shirly <1015885915@qq.com>
2019-11-21 16:09:35 +0800

    init student deomo

---
+++ .history/src/helpers/map_20191121153637.js
@@ -0,0 +1,9 @@
+import polyline from 'polyline'
+
+export const getCoordinates = (response) => {
+  if (response.data.routes[0] && response.data.routes[0].geometry) {
+    const geoData = polyline.toGeoJSON(response.data.routes[0].geometry);
+    return geoData.coordinates.map(coordinate => coordinate.reverse());
+  }
+  return [];
+};
